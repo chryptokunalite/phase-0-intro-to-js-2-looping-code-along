c306100007e931b37d3168c9290d7fe453f021fb
Allan Saenz <allan.d.saenz@gmail.com>
2023-05-11 21:11:46 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+const name = ["Charlie", "Samip", "Ali"]
+let event = "birthday"
+
+function writeCards(name, event){
+    const empty = []
+
+    for (let i = 0; i < name.length; i++){
+        console.log(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+        empty.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return empty
+} 
+
+function countDown(){
+let countdown = 10;
+while (countdown >= 0){
+    console.log(countdown--)
+}
+}
